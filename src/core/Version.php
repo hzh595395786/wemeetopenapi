e6bfb142a205ccc49725977fe7fbd896556b9f76
allenzhhou <allenzhhou@tencent.com>
2024-11-14 15:45:04 +0800

    1.上传sdk文件

--- src/core/Version.php
+++ src/core/Version.php
@@ -13,7 +13,7 @@ namespace wemeet\openapi\core;
 class Version {
 
     // Version SDK 版本
-    public $version = "openapi-sdk-php/v1.0.0.76";
+    public $version = "openapi-sdk-php/v1.0.1";
 
     public function getVersion() {
         return $this->version;
