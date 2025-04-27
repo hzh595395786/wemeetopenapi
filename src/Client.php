dd8208e96e50d2f90bad6ded786e65870dd5a283
allenzhhou <allenzhhou@tencent.com>
2025-04-27 18:19:17 +0800

    1.删除无用代码

--- src/Client.php
+++ src/Client.php
@@ -20,7 +20,6 @@ use wemeet\openapi\service\user_manager\api\UserManagerApi;
 use wemeet\openapi\service\records\api\RecordsApi;
 use wemeet\openapi\service\record_intelligence\api\RecordIntelligenceApi;
 use wemeet\openapi\service\meeting_guest\api\MeetingGuestApi;
-use wemeet\openapi\service\pstn\api\PstnApi;
 use wemeet\openapi\service\meeting_room\api\MeetingRoomApi;
 use wemeet\openapi\service\layout\api\LayoutApi;
 
@@ -34,7 +33,6 @@ class Client {
     private RecordsApi $records;
     private RecordIntelligenceApi $record_intelligence;
     private MeetingGuestApi $meeting_guest;
-    private PstnApi $pstn;
     private MeetingRoomApi $meeting_room;
     private LayoutApi $layout;
 
@@ -60,7 +58,6 @@ class Client {
         $this->records = new RecordsApi($this->config);
         $this->record_intelligence = new RecordIntelligenceApi($this->config);
         $this->meeting_guest = new MeetingGuestApi($this->config);
-        $this->pstn = new PstnApi($this->config);
         $this->meeting_room = new MeetingRoomApi($this->config);
         $this->layout = new LayoutApi($this->config);
         return $this;
@@ -86,9 +83,6 @@ class Client {
     }
     public function MeetingGuest() {
         return $this->meeting_guest;
-    }
-    public function Pstn() {
-        return $this->pstn;
     }
     public function MeetingRoom() {
         return $this->meeting_room;
